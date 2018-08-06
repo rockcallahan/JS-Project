c77fa0e3bf3bfc8e02d2194ca9b5be467208af33
unknown <pritamrox5@gmail.com>
2018-08-06 22:31:20 +0530

    added a bunch

---
+++ l1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout<<"this is c++ bitch!";
+    return 0;
+}
--- n1.java
+++ n1.java
@@ -1,6 +1,9 @@
-class n1{
-    main()
-    {
-        
+import java.util.function.Function;
+public class n1{
+    public static void main(String[] args) {
+        String x = "This is java Motherfucker";
+        System.out.println(x);
+        Function<Integer,Integer> f=(a)-> a+1;
+        System.out.println(f);
     }
 }
